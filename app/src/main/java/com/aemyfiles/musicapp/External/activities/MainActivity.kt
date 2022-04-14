50186d870049b77632ba785fe0993e06d6a687d7
Bùi Đình Hải <hai.bd.hp@gmail.com>
2022-04-14 23:21:36 +0700

    test update seekbar
    ====hai.bui1====

--- app/src/main/java/com/aemyfiles/musicapp/External/activities/MainActivity.kt
+++ app/src/main/java/com/aemyfiles/musicapp/External/activities/MainActivity.kt
@@ -8,7 +8,6 @@ import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
@@ -18,22 +17,27 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aemyfiles.musicapp.Domain.AudioDatabase
 import com.aemyfiles.musicapp.Domain.AudioInfo
 import com.aemyfiles.musicapp.Domain.AudioRepository
 import com.aemyfiles.musicapp.External.adapter.ShowListSongAdapter
+import com.aemyfiles.musicapp.External.utils.MediaManager
+import com.aemyfiles.musicapp.External.utils.Permission
 import com.aemyfiles.musicapp.Presenter.AudioViewModel
 import com.aemyfiles.musicapp.Presenter.AudioViewModelFactory
 import com.aemyfiles.musicapp.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import java.lang.Runnable
 
 class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener {
     lateinit var audioViewModel: AudioViewModel
     lateinit var player: MediaPlayer
     private var listSongs = ArrayList<AudioInfo>()
-    private var mPosition: Int = 0;
-    private val mHanlder: Handler = Handler()
+    private var mPosition: Int = -1;
+//    private val mHanlder: Handler = Handler()
 
     companion object {
         const val REQUEST_CODE: Int = 200
@@ -46,8 +50,6 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
         val audioRepository = AudioRepository(AudioDatabase(this))
         val factory = AudioViewModelFactory(audioRepository)
         audioViewModel = ViewModelProvider(this, factory).get(AudioViewModel::class.java)
-
-
         initView()
     }
 
@@ -64,8 +66,8 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
         }
 
         button_get_data.setOnClickListener {
-            if (isPermissionsAllowed()) getData()
-            else askForPermissions()
+            if (Permission.isPermissionsAllowed(this)) getData()
+            else Permission.askForPermissions(this)
         }
         audioViewModel.getAllAudio().observe(this, Observer {
             mAdapter.setData(it)
@@ -76,11 +78,13 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
                 player.pause()
                 play.setImageResource(R.drawable.ic_play)
                 Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show()
+
             } else {
                 player.start()
                 play.setImageResource(R.drawable.ic_pause)
                 Toast.makeText(this, "play", Toast.LENGTH_SHORT).show()
             }
+
         }
 
         next.setOnClickListener {
@@ -98,6 +102,7 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
         seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 player.seekTo(i)
+                if (player.isPlaying) play.setImageResource(R.drawable.ic_pause)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
@@ -114,76 +119,41 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
             //updateSeekbar()
             seekBar.max = player.duration
             tv_time_left.setText("00:00")
-            tv_time_right.setText("${player.duration/60000}:${player.duration%60000}")
+            tv_time_right.setText("${player.duration/60000}:${(player.duration%60000)/1000}")
         } catch (e: java.lang.Exception) {
 
         }
     }
 
-    @SuppressLint("Range")
-    private fun getDataFromMedia(): List<AudioInfo> {
-        val list = ArrayList<AudioInfo>()
-        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        val projection = arrayOf(
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.DATA,
-            MediaStore.Audio.Media.ALBUM_ID,
-            MediaStore.Audio.Media.ALBUM,
-            MediaStore.Audio.Media.ARTIST_ID,
-            MediaStore.Audio.Media.ARTIST,
-            MediaStore.Audio.Media.TITLE,
-            MediaStore.Audio.Media.DURATION,
-            MediaStore.Audio.Media.SIZE,
-            MediaStore.Audio.Media.DATE_ADDED,
-            MediaStore.Audio.Media.DISPLAY_NAME
-        )
+    private fun getData() {
+        val listSong = MediaManager.getDataFromMedia(this)
+        listSong?.let {
+            val allSong = it.toMutableList()
+            allSong.forEach { it -> audioViewModel.insert(it) }
+        }
+        Log.d("hai.bui1", "onCreate: ")
+    }
 
-        val cursor = applicationContext.contentResolver.query(uri, projection, null, null, null)
+    private fun updateSeekbar() {
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                try {
+                    if (player.isPlaying) {
+                        seekBar.progress = player.currentPosition
+                        handler.postDelayed(this, 15)
+                    }
+                } catch (e: java.lang.Exception) {
 
-        if (cursor != null) {
-            while (cursor.moveToNext()) {
-                val dur = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
-                val size = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE))
-                if (dur / 1000 < 60 || size / 1000000 < 0) {
-                    continue
                 }
-                val songId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
-                val path = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
-                val title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
-                val albumId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID))
-                val album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
-                val artirstId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID))
-                val artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
-                val date = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED))
-                val song = AudioInfo(songId, path, albumId, album, artirstId, artist, dur, size, date, title)
-                list.add(song)
             }
-            cursor.close()
-        }
-        return list
-    }
-
-    private fun isPermissionsAllowed(): Boolean {
-        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun askForPermissions(): Boolean {
-        if (!isPermissionsAllowed()) {
-                ActivityCompat.requestPermissions(
-                    this as Activity,
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    REQUEST_CODE
-                )
-            return false
-        }
-        return true
+        }, 15)
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
@@ -193,27 +163,18 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
                     getData()
                 } else {
                     // permission is denied, you can ask for permission again, if you want
-                    askForPermissions()
+                    Permission.askForPermissions(this)
                 }
                 return
             }
         }
     }
 
-    private fun getData() {
-        val listSong = getDataFromMedia()
-        listSong?.let {
-            val allSong = it.toMutableList()
-            allSong.forEach { it -> audioViewModel.insert(it) }
-        }
-        Log.d("hai.bui1", "onCreate: ")
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
             //save image to shared preference
-                getData()
+            getData()
         }
     }
 
@@ -233,22 +194,16 @@ class MainActivity : AppCompatActivity(), ShowListSongAdapter.AudioClickListener
         //updateSeekbar()
         seekBar.max = player.duration
         tv_time_left.setText("00:00")
-        tv_time_right.setText("${player.duration/60000}:${player.duration%60000}")
+        tv_time_right.setText("${player.duration/60000}:${(player.duration%60000)/1000}")
         play.setImageResource(R.drawable.ic_pause)
-    }
 
-    fun updateSeekbar() {
-        mHanlder.postDelayed(runnable, 1000)
-    }
-
-    var runnable: Runnable = object : Runnable {
-        override fun run() {
-            if (player.isPlaying) {
-                tv_time_left!!.text = "${player.currentPosition/60000}:${player.currentPosition%60000}"
-                seekBar!!.progress = player.currentPosition
-                mHanlder.postDelayed(this, 1000)
+        lifecycleScope.launch {
+            while (true) {
+                delay(1000)
+                seekBar.progress = player.currentPosition
             }
         }
     }
+
 
 }
